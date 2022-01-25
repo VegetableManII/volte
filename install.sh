9ad6bffe7674b0f468d120df86645b3b2a011e8e
jiaoxueming001 <jiaoxueming001@ke.com>
2022-01-25 12:31:34 +0800

    add panic recover

--- install.sh
+++ install.sh
@@ -5,7 +5,12 @@ yum install git
 ## 安装go
 wget https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
 tar -C /usr/local -xzvf  ./go1.17.2.linux-amd64.tar.gz
+mkdir -p /root/go/src
+mkdir -p /root/go/bin
+mkdir -p /root/go/mod
 export PATH=$PATH:/usr/local/go/bin
+export GOROOT=/usr/local/go
+export GOPATH=/root/go
 
 go version
 
