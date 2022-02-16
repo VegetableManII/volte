6557f3ad62d6ea3351b06d3f34c0272f93996735
jiaoxueming001 <jiaoxueming001@ke.com>
2022-02-16 12:10:07 +0800

    bearer

--- install.sh
+++ install.sh
@@ -6,20 +6,14 @@ yum localinstall https://dev.mysql.com/get/mysql57-community-release-el7-9.noarc
 ## 安装go
 wget https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
 tar -C /usr/local -xzvf  ./go1.17.2.linux-amd64.tar.gz
-mkdir -p /root/go/src
-mkdir -p /root/go/bin
-mkdir -p /root/go/mod
-echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile
-echo 'export GOROOT=/usr/local/go' >> /etc/profile
-echo 'export GOPATH=/root/go' >> /etcprofile
-
+mkdir -p /root/go/src & mkdir -p /root/go/bin & mkdir -p /root/go/mod
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile & echo 'export GOROOT=/usr/local/go' >> /etc/profile & echo 'export GOPATH=/root/go' >> /etc/profile
+source /etc/profile
 go version
 
 ## 拉取项目
 git clone https://github.com/VegetableManII/volte.git
 
 cd volte && git checkout 3-ims-logic-layer
-
-make
 
 sh ./run.sh
