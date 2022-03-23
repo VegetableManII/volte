295a4d4c0a8d841746ab154f6f2448aae6fe4795
VegetableManII <1343739480@qq.com>
2022-03-23 14:54:04 +0800

    I m finally fucking done code refactoring

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 go build ./entity/hss
 go build ./entity/i-cscf
-go build ./entity/mme
+go build ./entity/s-cscf
 go build ./entity/p-cscf
 go build ./entity/pgw
-go build ./entity/s-cscf
+
 
 
 nohup ./hss -f ./config.yml &
-nohup ./mme -f ./config.yml &
 nohup ./s-cscf -f ./config.yml &
 nohup ./i-cscf -f ./config.yml &
 nohup ./p-cscf -f ./config.yml &
