3b3ffc77cb7f870343ad889a471bb890a7bc85f0
VegetableManII <1343739480@qq.com>
2022-03-24 18:46:35 +0800

    I m finally fucking done code refactoring

--- build.sh
+++ build.sh
@@ -5,11 +5,9 @@ go build ./entity/p-cscf
 go build ./entity/pgw
 
 kill -9 $(ps aux|grep "./s-cscf -f ./config.yml"|awk 'NR==1{ print $2 }')
-kill -9 $(ps aux|grep "./s-cscf -f ./config.yml"|awk 'NR==1{ print $2 }')
 kill -9 $(ps aux|grep "./i-cscf -f ./config.yml"|awk 'NR==1{ print $2 }')
 kill -9 $(ps aux|grep "./p-cscf -f ./config.yml"|awk 'NR==1{ print $2 }')
 kill -9 $(ps aux|grep "./pgw -f ./config.yml"|awk 'NR==1{ print $2 }')
-
 
 nohup ./hss -f ./config.yml &
 nohup ./s-cscf -f ./config.yml &
