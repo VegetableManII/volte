a2b01696fde3dc4b797692e40b93b34e72c2689b
VegetableManII <1343739480@qq.com>
2022-03-29 18:09:44 +0800

    -

--- build.sh
+++ build.sh
@@ -15,13 +15,13 @@ go build ./entity/p-cscf
 go build ./entity/pgw
 
 
-kill -9 $(ps aux|grep "./hss -d $domain -f ./config.yml"|awk 'NR==1{ print $2 }')
+kill -9 $(ps aux|grep "./hss"|awk 'NR==1{ print $2 }')
 kill -9 $(ps aux|grep "./s-cscf -d $domain -f ./config.yml"|awk 'NR==1{ print $2 }')
 kill -9 $(ps aux|grep "./i-cscf -d $domain -f ./config.yml"|awk 'NR==1{ print $2 }')
 kill -9 $(ps aux|grep "./p-cscf -d $domain -f ./config.yml"|awk 'NR==1{ print $2 }')
 kill -9 $(ps aux|grep "./pgw -d $domain -f ./config.yml"|awk 'NR==1{ print $2 }')
 
-nohup ./hss -d $domain -f ./config.yml &
+nohup ./hss &
 nohup ./s-cscf -d $domain -f ./config.yml &
 nohup ./i-cscf -d $domain -f ./config.yml &
 nohup ./p-cscf -d $domain -f ./config.yml &
