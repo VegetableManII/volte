dc41159e85419eeba8a3723b4c62ed2afd80d0ef
VegetableManII <1343739480@qq.com>
2022-04-02 17:02:47 +0800

    -

--- build.sh
+++ build.sh
@@ -19,11 +19,8 @@ kill -9 $(ps aux|grep "./hss"|grep -v grep|awk 'NR==1{ print $2 }')
 kill -9 $(ps aux|grep "./s-cscf -d $domain -f ./config.yml"|grep -v grep|awk 'NR==1{ print $2 }')
 kill -9 $(ps aux|grep "./i-cscf -d $domain -f ./config.yml"|grep -v grep|awk 'NR==1{ print $2 }')
 kill -9 $(ps aux|grep "./p-cscf -d $domain -f ./config.yml"|grep -v grep|awk 'NR==1{ print $2 }')
-kill -9 $(ps aux|grep "./pgw -d $domain -f ./config.yml"|grep -v grep|awk 'NR==1{ print $2 }')
 
 nohup ./hss &
 nohup ./s-cscf -d $domain -f ./config.yml &
 nohup ./i-cscf -d $domain -f ./config.yml &
 nohup ./p-cscf -d $domain -f ./config.yml &
-nohup ./pgw -d $domain -f ./config.yml &
-
