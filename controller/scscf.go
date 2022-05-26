d1f3954d177950199b4a22524c47544dd4924dbc
VegetableManII <1343739480@qq.com>
2022-05-26 13:30:01 +0800

    m

--- controller/scscf.go
+++ controller/scscf.go
@@ -188,8 +188,8 @@ func (s *S_CscfEntity) SIPRESPONSEF(ctx context.Context, pkg *modules.Package, u
 	sipresp.Header.Via.RemoveFirst()
 	sipresp.Header.MaxForwards.Reduce()
 	via2, _ := sipresp.Header.Via.FirstAddrInfo()
-	// 当前跳为s-cscf，下一跳不是s-cscf，则说明响应来自另一个域,更新无线接入点
-	if strings.Contains(via1, "s-cscf") && !strings.Contains(via2, "s-cscf") {
+	// 当前跳为s-cscf，下一跳是p-cscf，则说明响应来自另一个域,更新无线接入点
+	if strings.Contains(via1, "s-cscf") && strings.Contains(via2, "p-cscf") {
 		caller := sipresp.Header.To.URI.Username
 		logger.Warn("主叫%v", caller)
 		user := s.sCache.getUserInfo(UeInfoPrefix + caller)
@@ -197,7 +197,7 @@ func (s *S_CscfEntity) SIPRESPONSEF(ctx context.Context, pkg *modules.Package, u
 		sipresp.Header.AccessNetworkInfo = user.AccessPoint
 	}
 	// 如果下一跳via包含s-cscf说明是另一个域的响应
-	if strings.Contains(via2, "s-cscf") {
+	if strings.Contains(via1, "i-cscf") && strings.Contains(via2, "s-cscf") {
 		logger.Info("[%v][%v] Receive From Other I-CSCF: \n%v", ctx.Value("Entity"), sip.ServerDomain, string(pkg.GetData()))
 		pkg.SetShortConn(config.Elements["OTHER"].ActualAddr)
 		pkg.Construct(modules.SIPPROTOCAL, modules.SipResponse, sipresp.String())
